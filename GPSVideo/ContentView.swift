ba8dc5ae0b09d5b69c6266bc32c63e38a500781c
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-03-26 14:50:25 -0500

    Initial Commit

---
+++ GPSVideo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GPSVideo
+//
+//  Created by Griffin Baker on 3/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GPSVideo/GPSVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSVideoApp.swift
+//  GPSVideo
+//
+//  Created by Griffin Baker on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GPSVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
